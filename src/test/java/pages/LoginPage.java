dcc57c0abb52d06c8fd337daa5dd72d98f9cca21
Abhi <abhishekghawalkar.busyqa@gmail.com>
2022-11-26 12:57:41 +0530

    first commit to add project

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,33 @@
+package pages;
+
+import org.openqa.selenium.JavascriptExecutor;
+
+import baseUtility.CommonUtility;
+
+public class LoginPage extends CommonUtility {
+
+	// Login Page xpaths
+	String loginForm = "//div/form[@autocomplete='on']";
+	String emailFld = "//label[normalize-space()='Enter Email/Mobile number']/preceding-sibling::input";
+	String passwordFld = "//label[normalize-space()='Enter Password']/preceding-sibling::input";
+	String loginBtn = "//button/span[text()='Login']";
+	String myAccountTxt = "//div[text()='My Account']";
+	// #######################################################################
+
+	public void loginFlipkart(String email, String password) {
+		navigateTo("https://www.flipkart.com/");
+		if (isElementDisplayed(loginForm)) {
+			typeIn(emailFld, email);
+			typeIn(passwordFld, password);
+			clickOn(loginBtn);
+		}
+
+		if (isElementDisplayed(myAccountTxt)) {
+			System.out.println("User logged in");
+		} else {
+			testStepFailed("User login failed");
+		}
+
+	}
+
+}
---
+++ src/test/java/testcases/LoginTest.java
@@ -0,0 +1,20 @@
+package testcases;
+
+import java.io.File;
+import java.util.Scanner;
+
+import org.openqa.selenium.WindowType;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+
+public class LoginTest extends LoginPage {
+
+	@Test
+	public void verifyLoginFuctionality() {
+		// Get user and pass
+		String email = getProperty("username");
+		String password = getProperty("password");
+		// Login
+		loginFlipkart(email, password);
+	}
+}
