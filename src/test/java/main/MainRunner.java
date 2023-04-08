25d3fca3c6d16f3d00670dbca2262356c17f976a
Abhi <abhishekghawalkar.busyqa@gmail.com>
2023-04-08 20:46:49 +0530

    demo

---
+++ src/test/java/main/MainRunner.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.TestNG;
+
+public class MainRunner {
+	static TestNG testng;
+	public static void main(String[] args) {
+		testng = new TestNG();
+		List<String> xmlList = new ArrayList<>();
+		xmlList.add("user.dir"+"/Suite.xml");
+		testng.setTestSuites(xmlList);
+		testng.run();
+	}
+
+}
