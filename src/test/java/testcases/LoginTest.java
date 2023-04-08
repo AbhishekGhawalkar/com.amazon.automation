a3993c0aa44c9e9f928444b28749acee9f03a25c
Abhi <abhishekghawalkar.busyqa@gmail.com>
2023-04-08 14:13:22 +0530

    new tests

--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -1,20 +1,16 @@
 package testcases;
 
-import java.io.File;
-import java.util.Scanner;
-
-import org.openqa.selenium.WindowType;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 
 public class LoginTest extends LoginPage {
 
-	@Test
-	public void verifyLoginFuctionality() {
-		// Get user and pass
-		String email = getProperty("username");
-		String password = getProperty("password");
-		// Login to the page
-		loginFlipkart(email, password);
-	}
+  @Test
+  public void verifyLoginFuctionality() {
+    // Get user and pass
+    String email = getProperty("username");
+    String password = getProperty("password");
+    // Login to the page
+    loginFlipkart(email, password);
+  }
 }
