5fc59391db09405e970cf0c3c130c09004d816a5
Abhi <abhishekghawalkar.busyqa@gmail.com>
2023-04-08 16:05:57 +0530

    test modify

--- src/test/java/testcases/TestClass.java
+++ src/test/java/testcases/TestClass.java
@@ -16,10 +16,6 @@ public class TestClass extends PageClass {
     String expUrl = "https://www.amazon.in";
     String productCategory = "Electronics";
     String productKeyword = "dell computers";
-    int minValue = 30000;
-    int maxValue = 50000;
-    int checkPricePageCount = 2;
-    int checkFiveStarProductsPageCount = 2;
     // Search keyword on google
     googleSearch(searchWord, expUrl);
     // Select and open expected url from results
@@ -28,13 +24,5 @@ public class TestClass extends PageClass {
     selectCategory(productCategory);
     // Search product name in searchbar
     seachProduct(productKeyword);
-    // Apply price filter to the result
-    applyPriceFilter(minValue, maxValue);
-    // Validate product prices are within range
-    checkPricesAreWithinRangeOnPages(checkPricePageCount, minValue, maxValue);
-    // Collect five star products from pages
-    allFiveStarProductsListOnPages(checkFiveStarProductsPageCount);
-    // Add first five-star product encountered into wishlist and validate
-    addFirstFiveStarProductToWishlist();
   }
 }
